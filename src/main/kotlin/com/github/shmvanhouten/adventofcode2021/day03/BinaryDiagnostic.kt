d7b62944ad6676a4d6c788aa36902415c4204edf
Sjoerd <svanhouten@gmail.com>
2021-12-04 13:29:56 +0100

    day 3 refactor

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2021/day03/BinaryDiagnostic.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2021/day03/BinaryDiagnostic.kt
@@ -5,56 +5,71 @@ fun findGammaAndEpsilonRate(lines: List<String>): Pair<Int, Int> {
     return gamma to getMaxBinary(lines).xor(gamma)
 }
 
-private fun getMaxBinary(lines: List<String>) =
-    lines[0].map { '1' }.joinToString("").toInt(2)
-
-fun findGammaRate(lines: List<String>): Int {
+private fun findGammaRate(lines: List<String>): Int {
     return 0.until(lines[0].length)
         .map { i -> lines.count { it[i] == '1' } }
-        .map { it > lines.size/2 }
-        .map { if(it) '1' else '0' }
+        .map { it > lines.size / 2 }
+        .map { boolToBit(it) }
         .joinToString("")
         .toInt(2)
 }
+
+private fun boolToBit(bool: Boolean) = if (bool) '1' else '0'
+
+private fun getMaxBinary(lines: List<String>) =
+    lines[0].map { '1' }.joinToString("").toInt(2)
 
 fun findOxygenGeneratorAndCO2Scrubber(lines: List<String>): Pair<Int, Int> {
     return findOxygenGeneratorRate(lines) to findC02ScrubberRate(lines)
 }
 
-fun findOxygenGeneratorRate(lines: List<String>): Int {
-    var i = 0
-    var remainingLines = lines
-    while (remainingLines.size > 1) {
-        val bit = findMostCommonBitAtIndex(remainingLines, i)
-        remainingLines = remainingLines.filter { it[i] == bit }
-        i++
+private fun findOxygenGeneratorRate(lines: List<String>): Int {
+    return filterBinaryStringForCriteria(lines) { bit, i, remainingLines ->
+        bit == findMostCommonBitAtIndex(remainingLines, i)
     }
-    return remainingLines[0].toInt(2)
 }
 
-fun findMostCommonBitAtIndex(lines: List<String>, index: Int) : Char {
-    val ones = lines.count { it[index] == '1' }
-    val zeroes = lines.count { it[index] == '0' }
+private fun findC02ScrubberRate(lines: List<String>): Int {
+    return filterBinaryStringForCriteria(lines) { bit, i, remainingLines ->
+        bit == findLeastCommonBitAtIndex(remainingLines, i)
+    }
+}
 
-    return if(ones >= zeroes) '1'
+private fun filterBinaryStringForCriteria(
+    lines: List<String>,
+    bitMatchesCriteria: (bit: Char, index: Int, lines: List<String>) -> Boolean
+): Int {
+    var remainingLines = lines
+    0.until(lines[0].length).forEach { i ->
+        remainingLines = remainingLines.filter { bitMatchesCriteria(it[i], i, remainingLines) }
+        if (remainingLines.size == 1) {
+            return remainingLines.first().toInt(2)
+        }
+    }
+    error("Something went wrong, no line won: remaining lines: $remainingLines")
+}
+
+private fun findMostCommonBitAtIndex(lines: List<String>, index: Int): Char {
+    val (ones, zeroes) = countOnesAndZeroes(lines, index)
+
+    return if (ones >= zeroes) '1'
     else '0'
 }
 
-fun findC02ScrubberRate(lines: List<String>): Int {
-    var i = 0
-    var remainingLines = lines
-    while (remainingLines.size > 1) {
-        val bit = findLeastCommonBitAtIndex(remainingLines, i)
-        remainingLines = remainingLines.filter { it[i] == bit }
-        i++
-    }
-    return remainingLines[0].toInt(2)
-}
+private fun findLeastCommonBitAtIndex(lines: List<String>, index: Int): Char {
+    val (ones, zeroes) = countOnesAndZeroes(lines, index)
 
-fun findLeastCommonBitAtIndex(lines: List<String>, index: Int) : Char {
-    val ones = lines.count { it[index] == '1' }
-    val zeroes = lines.count { it[index] == '0' }
-
-    return if(ones < zeroes) '1'
+    return if (ones < zeroes) '1'
     else '0'
 }
+
+private fun countOnesAndZeroes(
+    lines: List<String>,
+    index: Int
+): Pair<Int, Int> {
+    return countOnesAndZeroes(lines.map { it[index] })
+}
+
+private fun countOnesAndZeroes(bits: List<Char>) =
+    bits.count { it == '1' } to
+            bits.count { it == '0' }
