0d8678fe30caccc46a36f67b32114a3d92a7e85b
Sjoerd <svanhouten@gmail.com>
2021-12-04 11:41:32 +0100

    day 4 refactor

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2021/day04/BingoGame.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2021/day04/BingoGame.kt
@@ -2,29 +2,24 @@ package com.github.shmvanhouten.adventofcode2021.day04
 
 class BingoGame(val boards: List<BingoBoard>) {
     fun findWinner(winningNumbers: List<Long>): Pair<BingoBoard, Long> {
-        var i = 0
-        while (boards.none { it.hasBingo() }) {
-            val winningNumber = winningNumbers[i]
+        winningNumbers.forEach { winningNumber ->
             boards.forEach { it.flip(winningNumber) }
             val winner = boards.find { it.hasBingo() }
             winner?.let { return it to winningNumber}
-            i++
         }
         error("winning bingoboard not found")
     }
 
     fun findLoser(winningNumbers: List<Long>): Pair<BingoBoard, Long> {
-        var i = 0
         val remainingBoards = boards.toMutableList()
-        while (true) {
-            val winningNumber = winningNumbers[i]
+        winningNumbers.forEach { winningNumber ->
             remainingBoards.forEach { it.flip(winningNumber) }
             val winner = removeWinnersFromTheBoard(remainingBoards)
             if(remainingBoards.isEmpty()) {
                 return winner!! to winningNumber
             }
-            i++
         }
+        error("losing bingoboard not found")
     }
 
     private fun removeWinnersFromTheBoard(remainingBoards: MutableList<BingoBoard>): BingoBoard? {
