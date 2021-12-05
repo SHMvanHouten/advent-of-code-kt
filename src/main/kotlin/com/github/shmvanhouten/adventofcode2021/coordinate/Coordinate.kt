d8c74ab96ca3e0d4880de590c12d7096b8d5a8d0
Sjoerd <svanhouten@gmail.com>
2021-12-05 13:22:05 +0100

    Day 5 refactor: created CoordinateProgression

--- src/main/kotlin/com/github/shmvanhouten/adventofcode2021/coordinate/Coordinate.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode2021/coordinate/Coordinate.kt
@@ -8,32 +8,6 @@ fun toCoordinate(input: String): Coordinate {
     return Coordinate(x, y)
 }
 
-operator fun Coordinate.rangeTo(otherCoordinate: Coordinate): List<Coordinate> {
-    return when {
-        x == otherCoordinate.x -> generateVerticalRange(otherCoordinate)
-        y == otherCoordinate.y -> generateHorizontalRange(otherCoordinate)
-        else -> generateDiagonalRange(otherCoordinate)
-    }
+operator fun Coordinate.rangeTo(otherCoordinate: Coordinate): CoordinateProgression {
+    return CoordinateProgression(this, otherCoordinate)
 }
-
-fun upOrDownProgression(n1: Int, n2: Int): IntProgression =
-    if (n1 <= n2) (n1..n2)
-    else n1.downTo(n2)
-
-private fun Coordinate.generateVerticalRange(otherCoordinate: Coordinate) =
-    upOrDownProgression(y, otherCoordinate.y)
-        .map { y -> Coordinate(x, y) }
-
-private fun Coordinate.generateHorizontalRange(otherCoordinate: Coordinate) =
-    upOrDownProgression(x, otherCoordinate.x)
-        .map { x -> Coordinate(x, y) }
-
-private fun Coordinate.generateDiagonalRange(otherCoordinate: Coordinate): List<Coordinate> {
-    val xRange = upOrDownProgression(x, otherCoordinate.x).toList()
-    val yRange = upOrDownProgression(y, otherCoordinate.y).toList()
-    if (xRange.size != yRange.size) error("Diagonal line is not 45 degrees! $xRange, $yRange")
-    return xRange
-        .zip(yRange)
-        .map { (x, y) -> Coordinate(x, y) }
-}
-
