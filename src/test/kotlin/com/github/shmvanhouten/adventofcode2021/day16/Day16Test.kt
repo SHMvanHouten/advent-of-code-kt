db79c8d2ebfbf34574525c841f5ec89cb8ca7566
Sjoerd <svanhouten@gmail.com>
2021-12-16 07:43:47 +0100

    day 16 not started yet

---
+++ src/test/kotlin/com/github/shmvanhouten/adventofcode2021/day16/Day16Test.kt
@@ -0,0 +1,41 @@
+package com.github.shmvanhouten.adventofcode2021.day16
+
+import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Day16Test {
+
+    @Nested
+    inner class Part1 {
+
+        @Test
+        internal fun `fixme`() {
+            assertThat(1, equalTo(1) )
+        }
+
+        @Test
+        internal fun `part 1`() {
+            assertThat(1, equalTo(1) )
+        }
+    }
+
+    @Nested
+    inner class Part2 {
+
+        @Test
+        internal fun `fixme`() {
+            assertThat(1, equalTo(1) )
+        }
+
+        @Test
+        internal fun `part 2`() {
+            assertThat(1, equalTo(1) )
+        }
+    }
+
+    private val input by lazy { readFile("/input-day16.txt")}
+
+}
