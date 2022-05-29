49409cc7720ca5f4de38f09e8dca16fcdfb1c82a
Sjoerd <svanhouten@gmail.com>
2022-05-29 18:17:51 +0200

    fixed warning

--- src/main/kotlin/com/github/shmvanhouten/adventofcode/utility/FileReader.kt
+++ src/main/kotlin/com/github/shmvanhouten/adventofcode/utility/FileReader.kt
@@ -5,7 +5,7 @@ object FileReader {
     fun readFile(relativePath: String): String{
         val stringBuilder = StringBuilder()
 
-        val inputStream = this::class.java.getResourceAsStream(relativePath)
+        val inputStream = this::class.java.getResourceAsStream(relativePath)?: error("could not read file")
         inputStream.bufferedReader()
             .useLines { lines ->
                 lines.forEach {
