b055e384ee59b471137722b5ff55e4759f71f794
Sjoerd <svanhouten@gmail.com>
2022-12-13 22:10:13 +0100

    day 13 refactor

--- kt2022/src/main/kotlin/com/github/shmvanhouten/adventofcode2022/day13/PacketOrdering.kt
+++ kt2022/src/main/kotlin/com/github/shmvanhouten/adventofcode2022/day13/PacketOrdering.kt
@@ -3,10 +3,9 @@ package com.github.shmvanhouten.adventofcode2022.day13
 import com.github.shmvanhouten.adventofcode.utility.strings.blocks
 
 fun findIndicesOfCorrectPacketPairs(input: String): List<Int> {
-    return input.blocks().mapIndexedNotNull { i, pair ->
-        if(orderedCorrectly(pair.lines())) i + 1
-        else null
-    }
+    return input.blocks().withIndex().filter {
+        sortedCorrectly(it.value.lines())
+    }.map { it.index + 1 }
 }
 
 fun sortedWithDividers(input: String): List<String> {
@@ -14,28 +13,28 @@ fun sortedWithDividers(input: String): List<String> {
         .filter { it.isNotBlank() }
         .plus("[[2]]")
         .plus("[[6]]")
-        .sortedWith { l, r -> compare(r, l) }
+        .sortedWith { l, r -> compare(l, r) }
 }
 
 fun List<String>.decoderKey(): Int {
     return (this.indexOf("[[2]]") + 1) * (this.indexOf("[[6]]") + 1)
 }
 
-private fun orderedCorrectly(pairs: List<String>): Boolean {
-    return orderedCorrectly(pairs[0], pairs[1])
+private fun sortedCorrectly(pairs: List<String>): Boolean {
+    return sortedCorrectly(pairs[0], pairs[1])
 }
 
-fun orderedCorrectly(l: String, r: String): Boolean {
+fun sortedCorrectly(l: String, r: String): Boolean {
     return when(compare(l, r)) {
-        -1 -> false
+        1 -> false
         else -> true
     }
 }
 
 fun compare(left: String, right: String): Int {
     if (left.isEmpty() && right.isEmpty()) return 0
-    if (left.isEmpty()) return 1
-    if (right.isEmpty()) return -1
+    if (left.isEmpty()) return -1
+    if (right.isEmpty()) return 1
     if (left.first() == '[' && right.first() != '[') return compare(left, right.addBracketsAroundFirst())
     if (left.first() != '[' && right.first() == '[') return compare(left.addBracketsAroundFirst(), right)
     if(left.first() == '[' && right.first() == '[') {
@@ -50,11 +49,10 @@ fun compare(left: String, right: String): Int {
     }
     val leftNr = left.substringBefore(',').toInt()
     val rightNr = right.substringBefore(',').toInt()
-    if (leftNr < rightNr) return 1
-    if (leftNr > rightNr) return -1
+    if (leftNr < rightNr) return -1
+    if (leftNr > rightNr) return 1
     if(!left.contains(',') && !right.contains(',')) return 0
     return compare(left.substringAfter(',', ""), right.substringAfter(',', ""))
-
 }
 
 private fun String.stringAfter(index: Int): String {
@@ -80,19 +78,3 @@ private fun String.findClosingBracketIndex(): Int {
     }
     error("no closing bracket found for $this")
 }
-
-private fun isAList(s: String) = s.first() == '['
-
-private fun String.hasNext(): Boolean = isNotBlank()
-
-interface Comparable {
-
-}
-
-class Nr(val value: Int) : Comparable {
-    fun toList(): List_ {
-        return List_(listOf(this))
-    }
-}
-
-class List_(val value: List<Comparable>) : Comparable
--- kt2022/src/test/kotlin/com/github/shmvanhouten/adventofcode2022/day13/Day13Test.kt
+++ kt2022/src/test/kotlin/com/github/shmvanhouten/adventofcode2022/day13/Day13Test.kt
@@ -17,7 +17,7 @@ class Day13Test {
                 [1]
                 [2]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -26,7 +26,7 @@ class Day13Test {
                 [2]
                 [1]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isFalse
+            assertThat(sortedCorrectly(input[0], input[1])).isFalse
         }
 
         @Test
@@ -35,7 +35,7 @@ class Day13Test {
                 [1]
                 [1,1]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -44,7 +44,7 @@ class Day13Test {
                 [1,1]
                 [1]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isFalse
+            assertThat(sortedCorrectly(input[0], input[1])).isFalse
         }
 
         @Test
@@ -53,14 +53,14 @@ class Day13Test {
                 []
                 [1]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
         internal fun `example pt 1, Compare {1,1,3,1,1} vs {1,1,5,1,1}`() {
             val input = """[1,1,3,1,1]
                 |[1,1,5,1,1]""".trimMargin().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -69,7 +69,7 @@ class Day13Test {
                 [[2]]
                 [[1]]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isFalse
+            assertThat(sortedCorrectly(input[0], input[1])).isFalse
         }
 
         @Test
@@ -78,7 +78,7 @@ class Day13Test {
                 [[1],[2,3,4]]
                 [[1],4]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -87,7 +87,7 @@ class Day13Test {
                 [1,[2,[3,[4,[5,6,7]]]],8,9]
                 [1,[2,[3,[4,[5,6,0]]]],8,9]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isFalse
+            assertThat(sortedCorrectly(input[0], input[1])).isFalse
         }
 
         @Test
@@ -96,7 +96,7 @@ class Day13Test {
                 [1,[2,4],3]
                 [1,[2,4],2]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isFalse
+            assertThat(sortedCorrectly(input[0], input[1])).isFalse
         }
 
         @Test
@@ -140,7 +140,7 @@ class Day13Test {
                 [[9,[0],[],5,6],[2],[],[1,[4,[7,4,3,8,0],[1,5,1,1],[3],7],[4,10,5,[4,10]]]]
             """.trimIndent().lines()
 
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -149,7 +149,7 @@ class Day13Test {
                 [[[[],2,[7,5,4,7,7],10],[[3],[2,8],1,6,[]],2],[[9,[3,6,5],[9,10,1,6]]],[],[[[3,10,5,6,0],[],[4,4,9,6,6],1,[8,4,7,1,0]]]]
                 [[1,[2,5,[10,6]],0,10],[[0,3]]]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0], input[1])).isTrue
+            assertThat(sortedCorrectly(input[0], input[1])).isTrue
         }
 
         @Test
@@ -158,7 +158,7 @@ class Day13Test {
                 [[]]
                 [[[]]]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0],input[1])).isTrue
+            assertThat(sortedCorrectly(input[0],input[1])).isTrue
         }
 
         @Test
@@ -167,7 +167,7 @@ class Day13Test {
                 [[[[7]],2]]
                 [[7,2],[],[[5,7,3],[],[[2,2],[],9,[2,6,7]]]]
             """.trimIndent().lines()
-            assertThat(orderedCorrectly(input[0],input[1])).isTrue
+            assertThat(sortedCorrectly(input[0],input[1])).isTrue
         }
 
         @Test
@@ -180,9 +180,17 @@ class Day13Test {
         }
 
         @Test
+        internal fun `deal with empty list`() {
+            val input = """
+                [[[[],2,[7,5,4,7,7],10],[[3],[2,8],1,6,[]],2],[[9,[3,6,5],[9,10,1,6]]],[],[[[3,10,5,6,0],[],[4,4,9,6,6],1,[8,4,7,1,0]]]]
+                [[1,[2,5,[10,6]],0,10],[[0,3]]]
+            """.trimIndent().lines()
+            assertThat(compare(input[0],input[1])).isEqualTo(-1)
+        }
+
+        @Test
         internal fun `part 1`() {
             assertThat(findIndicesOfCorrectPacketPairs(input).sum()).isEqualTo(6070)
-            // 4820 too low
         }
     }
 
@@ -245,8 +253,6 @@ class Day13Test {
         @Test
         internal fun `part 2`() {
             val sorted = sortedWithDividers(input)
-            assertThat(sorted.decoderKey()).isLessThan(21978)
-            assertThat(sorted.decoderKey()).isEqualTo(20160)
             assertThat(sorted.decoderKey()).isEqualTo(20758)
         }
     }
