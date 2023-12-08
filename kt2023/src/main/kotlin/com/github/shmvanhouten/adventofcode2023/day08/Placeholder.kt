26add5fcbf178cb196fb4d01294c0cc193e7dde4
Sjoerd <svanhouten@gmail.com>
2023-12-08 08:13:58 +0100

    day 8 pt2

--- kt2023/src/main/kotlin/com/github/shmvanhouten/adventofcode2023/day08/Placeholder.kt
+++ kt2023/src/main/kotlin/com/github/shmvanhouten/adventofcode2023/day08/Placeholder.kt
@@ -3,8 +3,8 @@ package com.github.shmvanhouten.adventofcode2023.day08
 import com.github.shmvanhouten.adventofcode.utility.strings.words
 
 
-fun toNetworkInstructions(instructions: String, network: String): NetworkInstructions {
-    return NetworkInstructions(
+fun toNetworkInstructions(instructions: String, network: String): NetworkInstructions1 {
+    return NetworkInstructions1(
         instructions.map { c ->
             when(c) {
                 'L' -> {it -> it.left}
@@ -12,7 +12,7 @@ fun toNetworkInstructions(instructions: String, network: String): NetworkInstruc
                 else -> throw NotImplementedError("")
             }
         },
-        network.lines().map { toElement(it) }.toMap()
+        network.lines().map { com.github.shmvanhouten.adventofcode2023.day08.part2.toElement(it) }.toMap()
     )
 }
 
@@ -21,9 +21,9 @@ fun toElement(line: String): Pair<String, Elements> {
     return key to Elements(v1, v2)
 }
 
-data class NetworkInstructions(val instructions: List<(Elements) -> String>, val network: Map<String, Elements>) {
-    fun traverseUntil(target: String): List<String> {
-        var current = "AAA"
+data class NetworkInstructions1(val instructions: List<(Elements) -> String>, val network: Map<String, Elements>) {
+    fun traverseUntil(target: String, start: String = "AAA"): List<String> {
+        var current = start
         val traversed = mutableListOf<String>()
         while (true) {
             instructions.forEach { take ->
--- kt2023/src/test/kotlin/com/github/shmvanhouten/adventofcode2023/day08/Day08Test.kt
+++ kt2023/src/test/kotlin/com/github/shmvanhouten/adventofcode2023/day08/Day08Test.kt
@@ -6,10 +6,15 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isGreaterThan
+import strikt.assertions.isLessThan
+import java.math.BigInteger
 
 class Day08Test {
 
-    private val exampleInput = """
+    @Nested
+    inner class Part1 {
+        private val exampleInput = """
         RL
 
         AAA = (BBB, CCC)
@@ -21,14 +26,12 @@ class Day08Test {
         ZZZ = (ZZZ, ZZZ)
     """.trimIndent()
 
-    @Nested
-    inner class Part1 {
-
         @Test
         internal fun `example 1`() {
             val (instructions, network) = exampleInput
                 .blocks()
-            val networkInstructions = toNetworkInstructions(instructions, network)
+            val networkInstructions =
+                toNetworkInstructions(instructions, network)
             expectThat(networkInstructions.traverseUntil("ZZZ").size)
                 .isEqualTo(2)
         }
@@ -37,23 +40,81 @@ class Day08Test {
         internal fun `part 1`() {
             val (instructions, network) = input
                 .blocks()
-            val networkInstructions = toNetworkInstructions(instructions, network)
+            val networkInstructions =
+                toNetworkInstructions(instructions, network)
             expectThat(networkInstructions.traverseUntil("ZZZ").size)
-                .isEqualTo(2)
+                .isEqualTo(13939)
         }
     }
 
     @Nested
     inner class Part2 {
 
+        private val exampleInput = """
+            LR
+
+            11A = (11B, xxx)
+            11B = (xxx, 11Z)
+            11Z = (11B, xxx)
+            22A = (22B, xxx)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            xxx = (xxx, xxx)
+        """.trimIndent()
+
+        // how long does it take to travel from 11A to any target ending in Z
+        // how long does it take to travel from 22A to any target ending in Z
+        // for any target ending in z: how long does it take to get the next z (map it)
+
         @Test
-        internal fun `fixme`() {
-            expectThat(1).isEqualTo(1)
+        internal fun `example 2`() {
+            val (instructions, network) = exampleInput.blocks()
+            val networkInstructions =
+                com.github.shmvanhouten.adventofcode2023.day08.part2.toNetworkInstructions(instructions, network)
+
+            expectThat(networkInstructions.findFirstPointWhereAllPathsHitTarget())
+                .isEqualTo(BigInteger("6"))
+        }
+
+        @Test
+        fun otherTest() {
+            val (instructions, network) = """
+                LR
+                
+                33A = (33B, xxx)
+                33B = (xxx, 33Z)
+                33Z = (33B, xxx)
+                11A = (11B, XXX)
+                11B = (XXX, 11C)
+                11C = (11D, XXX)
+                11D = (XXX, 11Z)
+                11Z = (11B, XXX)
+                22A = (22B, XXX)
+                22B = (22C, 22C)
+                22C = (22Z, 22Z)
+                22Z = (22B, 22B)
+                XXX = (XXX, XXX)
+            """.trimIndent().blocks()
+
+            val networkInstructions =
+                com.github.shmvanhouten.adventofcode2023.day08.part2.toNetworkInstructions(instructions, network)
+
+            expectThat(networkInstructions.findFirstPointWhereAllPathsHitTarget())
+                .isEqualTo(BigInteger("12"))
         }
 
         @Test
         internal fun `part 2`() {
-            expectThat(1).isEqualTo(1)
+            val (instructions, network) = input.blocks()
+            val networkInstructions =
+                com.github.shmvanhouten.adventofcode2023.day08.part2.toNetworkInstructions(instructions, network)
+
+            println(BigInteger("11206957317755125787748971").divide(BigInteger("263")))
+            expectThat(networkInstructions.findFirstPointWhereAllPathsHitTarget())
+                .isGreaterThan(BigInteger("33865167419"))
+                .isLessThan(BigInteger("42612005010475763451517"))
+                .isEqualTo(BigInteger.ZERO)
         }
     }
 
