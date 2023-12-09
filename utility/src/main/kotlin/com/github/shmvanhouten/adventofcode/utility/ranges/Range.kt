bb18ec1b037fb378c5d59e1a79fa0f6035cbd73a
Sjoerd <svanhouten@gmail.com>
2023-12-09 12:31:16 +0100

    overlap ranges

--- utility/src/main/kotlin/com/github/shmvanhouten/adventofcode/utility/ranges/Range.kt
+++ utility/src/main/kotlin/com/github/shmvanhouten/adventofcode/utility/ranges/Range.kt
@@ -1,6 +1,58 @@
 package com.github.shmvanhouten.adventofcode.utility.ranges
 
+import kotlin.math.min
+
 fun IntRange.contains(other: IntRange): Boolean {
     return this.first <= other.first
             && this.last >= other.last
 }
+
+fun LongRange.overlaps(other: LongRange): Boolean =
+    this.first in other || this.last in other || other.first in this || other.last in this
+
+/**
+ * Returns
+ * - a ( empty or size 1) list of overlapping where this range and the other overlap
+ * - a (size 0 - 2) list of ranges where this range does _not_ overlap the other
+ *
+ * **ignores where the other range does not overlap this range!**
+ */
+fun LongRange.splitOverlapsOn(other: LongRange): OverlapSplit {
+    return if (!this.overlaps(other)) OverlapSplit(emptyList(), listOf(this))
+    else {
+        val notOverlapping = mutableListOf<LongRange>()
+        val overlapping = mutableListOf<LongRange>()
+        if (this.first < other.first) {
+            notOverlapping += this.first.until(min(this.last + 1, other.first))
+            overlapping += other.first..minOf(this.last, other.last)
+            if (other.last <= this.last) {//this completely contains other
+                notOverlapping += (other.last + 1)..this.last
+            }
+        } else {
+            overlapping += this.first..min(other.last, this.last)
+            notOverlapping += (other.last + 1)..this.last
+        }
+        OverlapSplit(overlapping.toList(), notOverlapping.toList())
+    }
+}
+
+fun LongRange.splitOverlapsOn(others: List<LongRange>): OverlapSplit {
+    return others.fold(OverlapSplit(emptyList(), listOf(this))) { acc, other ->
+        val (overlapping, notOverlapping) = acc.notOverlapping.map { it.splitOverlapsOn(other) }.flatten()
+        OverlapSplit(
+            overlapping = acc.overlapping + overlapping,
+            notOverlapping = notOverlapping
+        )
+    }
+}
+
+data class OverlapSplit(val overlapping: List<LongRange>, val notOverlapping: List<LongRange>)
+
+private fun List<OverlapSplit>.flatten(): OverlapSplit {
+    return OverlapSplit(
+        this.flatMap { it.overlapping },
+        this.flatMap { it.notOverlapping }
+    )
+}
+
+fun emptyOverlapSplit() = OverlapSplit(emptyList(), emptyList())
---
+++ utility/src/test/kotlin/com/github/shmvanhouten/adventofcode/utility/ranges/RangeOverlapTest.kt
@@ -0,0 +1,140 @@
+package com.github.shmvanhouten.adventofcode.utility.ranges
+
+import org.junit.jupiter.api.Test
+import strikt.api.expect
+import strikt.assertions.*
+
+class RangeOverlapTest {
+
+    @Test
+    fun `1-3 does not overlap with 4-5`() {
+        val (overlapping, notOverlapping) = (1L..3).splitOverlapsOn(4L..5)
+        expect {
+            that(overlapping).isEmpty()
+            that(notOverlapping)
+                .hasSize(1)
+                .first().isEqualTo(1L..3)
+
+        }
+    }
+
+    @Test
+    fun `5-6 does not overlap with 3-4`() {
+        val (overlapping, notOverlapping) = (5L..6).splitOverlapsOn(3L..4)
+        expect {
+            that(overlapping).isEmpty()
+            that(notOverlapping)
+                .hasSize(1)
+                .first().isEqualTo(5L..6)
+
+        }
+    }
+
+    @Test
+    fun `1-4 overlaps with 4-5 on 4`() {
+        // 1--4..
+        // ...45.
+        val (overlapping, notOverlapping) = (1L..4).splitOverlapsOn(4L..5)
+        expect {
+            that(overlapping).hasSize(1)
+                .first().isEqualTo(4L..4)
+            that(notOverlapping).hasSize(1)
+                .first().isEqualTo(1L..3)
+        }
+    }
+
+    @Test
+    fun `5-6 overlaps with 3-5 on 5`() {
+        // ....56...
+        // ..3-5....
+        val (overlapping, notOverlapping) = (5L..6).splitOverlapsOn(3L..5)
+        expect {
+            that(overlapping).hasSize(1)
+                .first().isEqualTo(5L..5)
+            that(notOverlapping).hasSize(1)
+                .first().isEqualTo(6L..6)
+        }
+    }
+
+    @Test
+    fun `5-9 overlaps with 3-6 on 5-6`() {
+        // ....5---9
+        // ..3--6...
+        // ....56...
+        // ......7-9
+        val (overlapping, notOverlapping) = (5L..9).splitOverlapsOn(3L..6)
+        expect {
+            that(overlapping).hasSize(1)
+                .first().isEqualTo(5L..6)
+            that(notOverlapping).hasSize(1)
+                .first().isEqualTo(7L..9)
+        }
+    }
+
+    @Test
+    fun `1-9 completely contains 4-5`() {
+        val (overlapping, notOverlapping) = (1L..9).splitOverlapsOn(4L..5)
+        expect {
+            that(overlapping).hasSize(1)
+                .first().isEqualTo(4L..5)
+            that(notOverlapping).hasSize(2)
+                .and {
+                   first().isEqualTo(1L..3)
+                   get(1).isEqualTo(6L..9)
+                }
+        }
+    }
+
+
+
+    @Test
+    fun `1--9 overlaps with 4-4 and 5-7 fully`() {
+        val (overlapping, notOverlapping) = (1L..9)
+            .splitOverlapsOn(listOf(4L..4, 5L..7))
+
+        expect {
+            that(overlapping)
+                .hasSize(2)
+                .and {
+                    first().isEqualTo(4L..4)
+                    get(1).isEqualTo(5L..7)
+                }
+            that(notOverlapping)
+                .hasSize(2)
+                .and {
+                    first().isEqualTo(1L..3)
+                    get(1).isEqualTo(8L..9)
+                }
+        }
+    }
+
+    @Test
+    fun `overlaps on 5-7 and not on 8-9`() {
+        val (overlapping, notOverlapping) = (5L..9)
+            .splitOverlapsOn(listOf(4L..4, 5L..7))
+        expect {
+            that(overlapping).hasSize(1)
+                .first().isEqualTo(5L..7)
+            that(notOverlapping).hasSize(1)
+                .and { first().isEqualTo(8L..9) }
+        }
+    }
+
+    @Test
+    fun `3--7 overlaps with 1-4 partially and 5-5 fully and 7-8 partially`() {
+        val (overlapping, notOverlapping) = (3L..7)
+            .splitOverlapsOn(listOf(1L..4, 5L..5, 7L..8))
+
+        expect {
+            that(overlapping).hasSize(3)
+                .and {
+                    first().isEqualTo(3L..4)
+                    get(1).isEqualTo(5L..5)
+                    get(2).isEqualTo(7L..7)
+                }
+
+            that(notOverlapping).hasSize(1)
+                .and { first().isEqualTo(6L..6) }
+        }
+    }
+}
