a98a840fcd623b2561eaec9b79376c3860266297
Sjoerd <svanhouten@gmail.com>
2023-12-09 06:16:09 +0100

    day 9 pt1

---
+++ kt2023/src/main/kotlin/com/github/shmvanhouten/adventofcode2023/day09/Placeholder.kt
@@ -0,0 +1,25 @@
+package com.github.shmvanhouten.adventofcode2023.day09
+
+import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
+import com.github.shmvanhouten.adventofcode.utility.strings.words
+
+fun main() {
+    readFile("/input-day09.txt")
+        .lines()
+        .map { toDataSet(it) }
+        .map { extrapolate(it) }
+        .onEach(::println)
+}
+
+fun extrapolate(line: List<Long>): Long {
+    val lineHistory = mutableListOf(line)
+    var nextLine = line.zipWithNext().map { it.second - it.first }
+    while (nextLine.any { it != 0L }) {
+        lineHistory += nextLine
+        nextLine = nextLine.zipWithNext().map { it.second - it.first }
+    }
+    return lineHistory.reversed().fold(0L) {acc, longs ->  acc + longs.last()}
+}
+
+fun toDataSet(it: String) = it.words().map { it.toLong() }
+
---
+++ kt2023/src/test/kotlin/com/github/shmvanhouten/adventofcode2023/day09/Day09Test.kt
@@ -0,0 +1,60 @@
+package com.github.shmvanhouten.adventofcode2023.day09
+
+import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class Day09Test {
+
+    val example = """
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+    """.trimIndent()
+    @Nested
+    inner class Part1 {
+
+        @Test
+        internal fun `line extrapolated is 18`() {
+            val line = toDataSet("0 3 6 9 12 15")
+            expectThat(extrapolate(line)).isEqualTo(18)
+        }
+
+        @Test
+        fun `example 1`() {
+            expectThat(
+            example.lines()
+                .map { toDataSet(it) }
+                .sumOf { extrapolate(it) }
+            ).isEqualTo(114)
+        }
+
+        @Test
+        internal fun `part 1`() {
+            expectThat(
+                input.lines()
+                    .map { toDataSet(it) }
+                    .sumOf { extrapolate(it) }
+            ).isEqualTo(114)
+        }
+    }
+
+    @Nested
+    inner class Part2 {
+
+        @Test
+        internal fun `fixme`() {
+            expectThat(1).isEqualTo(1)
+        }
+
+        @Test
+        internal fun `part 2`() {
+            expectThat(1).isEqualTo(1)
+        }
+    }
+
+    private val input by lazy { readFile("/input-day09.txt")}
+
+}
