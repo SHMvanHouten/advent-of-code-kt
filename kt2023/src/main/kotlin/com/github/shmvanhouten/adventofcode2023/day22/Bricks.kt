dc3314c1e9858e8c1d1e541b8ba0c3b791fd3117
Sjoerd <svanhouten@gmail.com>
2023-12-22 11:22:56 +0100

    day 22 refactor for part 2

--- kt2023/src/main/kotlin/com/github/shmvanhouten/adventofcode2023/day22/Bricks.kt
+++ kt2023/src/main/kotlin/com/github/shmvanhouten/adventofcode2023/day22/Bricks.kt
@@ -4,39 +4,45 @@ import com.github.shmvanhouten.adventofcode.utility.coordinate.Coordinate3d
 import com.github.shmvanhouten.adventofcode.utility.coordinate.CoordinateProgression
 import com.github.shmvanhouten.adventofcode.utility.coordinate.coordinate3d.Coordinate3DProgression
 
-fun safeToBeDisintegrated(bricks: List<Brick>): List<Brick> {
+fun safeToBeDisintegrated(bricks: List<DroppedBrick>, supportMap: Map<DroppedBrick, List<DroppedBrick>>): List<DroppedBrick> {
     val bricksByLayer = bricks.groupBy { it.locations.start.z }
-    val bricksByTopLayer = bricks.groupBy { it.locations.end.z }
     return bricks.filter { brick ->
-        val bricksItSupports = bricksByLayer[brick.locations.end.z + 1]?.filter { it.isInTheSamePlaneAs(brick) }?: emptyList()
-        bricksItSupports.isEmpty() || bricksItSupports.all { it.isSupportedByMoreThan1Brick(bricksByTopLayer) }
+        val bricksItSupports = bricksByLayer[brick.locations.end.z + 1]?.filter { brick in it.supportedBy }?: emptyList()
+        bricksItSupports.isEmpty() || bricksItSupports.all { it.supportedBy.size >= 2 }
     }
 }
 
-fun drop(bricks: List<Brick>): List<Brick> {
-    val droppedBricks = mutableListOf<Brick>()
+fun drop(bricks: List<Brick>): Pair<List<DroppedBrick>, Map<DroppedBrick, List<DroppedBrick>>> {
+    val droppedBricks = mutableListOf<DroppedBrick>()
+    val supportMap = mutableMapOf<DroppedBrick, List<DroppedBrick>>()
     bricks.sortedBy { it.locations.start.z }
         .forEach { brick ->
             val bricksBelow = droppedBricks.filter { it.isInTheSamePlaneAs(brick) }
-            if(bricksBelow.isEmpty()) droppedBricks += brick.droppedTo(1)
-            else droppedBricks += brick.droppedTo(
-                bricksBelow.maxOf { it.locations.end.z } + 1
-            )
+            droppedBricks += if(bricksBelow.isEmpty()) brick.droppedTo(1, emptyList())
+            else {
+                val maxZ = bricksBelow.maxOf { it.locations.end.z }
+                brick.droppedTo(
+                    maxZ + 1,
+                    bricksBelow.filter { it.locations.end.z == maxZ }
+                )
+            }
 
         }
-    return droppedBricks
+    return droppedBricks to supportMap
 }
 
 
-data class Brick(val locations: Coordinate3DProgression) {
+open class Brick(val locations: Coordinate3DProgression, val id: Int) {
     private val plane: CoordinateProgression = locations.start.on2dPlane..locations.end.on2dPlane
 
-    fun droppedTo(z: Int): Brick {
-        return Brick(
+    fun droppedTo(z: Int, supportingBricks: List<DroppedBrick>): DroppedBrick {
+        return DroppedBrick(
             Coordinate3DProgression(
                 locations.start.copy(z = z),
                 locations.end.copy(z = z + (locations.end.z - locations.start.z))
-            )
+            ),
+            supportingBricks,
+            id
         )
     }
 
@@ -45,20 +51,44 @@ data class Brick(val locations: Coordinate3DProgression) {
         // todo; make more efficient
     }
 
-    fun isSupportedByMoreThan1Brick(bricksByTopLayer: Map<Int, List<Brick>>): Boolean {
-        val layer = this.locations.start.z
-        return (bricksByTopLayer[layer - 1]?.count {this.isInTheSamePlaneAs(it)}?:0) >= 2
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Brick
+
+        return id == other.id
     }
+
+    override fun hashCode(): Int {
+        return locations.hashCode()
+    }
+
+    override fun toString(): String {
+        return "$id : $locations"
+    }
+
+
 }
 
-private fun String.coordinate3d(): Coordinate3d {
+data class DroppedBrick(
+    private val range: Coordinate3DProgression,
+    val supportedBy: List<DroppedBrick>,
+    val identifier: Int
+): Brick(range, identifier)
+
+fun String.coordinate3d(): Coordinate3d {
     val (x, y, z) = this.split(',')
     return Coordinate3d(x.toInt(), y.toInt(), z.toInt())
 }
 
 fun parse(raw: String): List<Brick> {
-    return raw.lines().map {
-        val(left, right) = it.split('~')
-        Brick(Coordinate3DProgression(left.coordinate3d(), right.coordinate3d()))
+    return raw.lines().mapIndexed { i, it ->
+        toBrick(it, i)
     }
+}
+
+fun toBrick(it: String, i: Int): Brick {
+    val (left, right) = it.split('~')
+    return Brick(Coordinate3DProgression(left.coordinate3d(), right.coordinate3d()), i)
 }
--- kt2023/src/test/kotlin/com/github/shmvanhouten/adventofcode2023/day22/Day22Test.kt
+++ kt2023/src/test/kotlin/com/github/shmvanhouten/adventofcode2023/day22/Day22Test.kt
@@ -1,9 +1,10 @@
 package com.github.shmvanhouten.adventofcode2023.day22
 
 import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
-import org.junit.jupiter.api.Disabled
+import com.github.shmvanhouten.adventofcode.utility.coordinate.coordinate3d.Coordinate3DProgression
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import strikt.api.expect
 import strikt.api.expectThat
 import strikt.assertions.hasSize
 import strikt.assertions.isEqualTo
@@ -26,22 +27,27 @@ class Day22Test {
         @Test
         fun `the ground is at 0, so a single brick will fall down to z = 1`() {
             val bricks = parse("1,0,7~1,1,7")
-            expectThat(drop(bricks))
-                .isEqualTo(parse("1,0,1~1,1,1"))
+            expectThat(drop(bricks).first)
+                .isEqualTo(
+                    listOf(DroppedBrick(
+                        Coordinate3DProgression("1,0,1".coordinate3d(), "1,1,1".coordinate3d()),
+                        emptyList(),
+                        0
+                    ))
+                )
         }
 
         @Test
-        @Disabled("Sorting made p1 fail")
         fun `an upright brick drops onto its lowest side`() {
             val bricks = parse("""
                 1,0,2~1,0,3
-                1,1,5~1,1,4
+                1,1,4~1,1,5
             """.trimIndent())
-            expectThat(drop(bricks))
-                .isEqualTo(parse("""
-                    1,0,1~1,0,2
-                    1,1,1~1,1,2
-                """.trimIndent()))
+            expectThat(drop(bricks).first)
+                .isEqualTo(listOf(
+                    DroppedBrick(to3dRange("1,0,1~1,0,2"), emptyList(), 0),
+                    DroppedBrick(to3dRange("1,1,1~1,1,2"), emptyList(), 1)
+                ))
         }
 
         @Test
@@ -50,11 +56,14 @@ class Day22Test {
                 1,0,5~1,1,5
                 1,1,3~1,1,3
             """.trimIndent())
-            expectThat(drop(bricks))
+            val dropped = drop(bricks).first
+            expectThat(dropped.map { it.toSimpleBrick() })
                 .isEqualTo(parse("""
-                    1,1,1~1,1,1
                     1,0,2~1,1,2
-                """.trimIndent()))
+                    1,1,1~1,1,1
+                """.trimIndent()).sortedBy { it.locations.start.z })
+            expectThat(dropped.first().supportedBy).hasSize(0)
+            expectThat(dropped[1].supportedBy).hasSize(1)
         }
 
         @Test
@@ -63,17 +72,18 @@ class Day22Test {
                 1,0,5~1,4,5
                 0,2,3~3,2,3
             """.trimIndent())
-            expectThat(drop(bricks))
+            expectThat(drop(bricks).first.map { it.toSimpleBrick() })
                 .isEqualTo(parse("""
-                    0,2,1~3,2,1
                     1,0,2~1,4,2
-                """.trimIndent()))
+                    0,2,1~3,2,1
+                """.trimIndent()).sortedBy { it.locations.start.z })
         }
 
         @Test
         internal fun `example bricks drop to`() {
             val bricks = parse(example)
-            expectThat(drop(bricks)).isEqualTo(parse("""
+            val dropped = drop(bricks).first
+            expectThat(dropped.map { it.toSimpleBrick() }).isEqualTo(parse("""
                 1,0,1~1,2,1
                 0,0,2~2,0,2
                 0,2,2~2,2,2
@@ -87,30 +97,28 @@ class Day22Test {
         @Test
         fun `example 1`() {
             val bricks = drop(parse(example))
-            expectThat(safeToBeDisintegrated(bricks))
-                .hasSize(5)
-                .isEqualTo(parse("""
-                    0,0,2~2,0,2
-                    0,2,2~2,2,2
-                    0,0,3~0,2,3
-                    2,0,3~2,2,3
-                    1,1,5~1,1,6
-                """.trimIndent()))
+            val safe = safeToBeDisintegrated(bricks.first, bricks.second)
+            expect {
+                that(safe).hasSize(5)
+                that(safe.map { it.id }).isEqualTo(listOf(1,2,3,4,6))
+            }
         }
 
         @Test
         internal fun `part 1`() {
             val bricks = drop(parse(input))
-            expectThat(safeToBeDisintegrated(bricks).size)
+            expectThat(safeToBeDisintegrated(bricks.first, bricks.second).size)
                 .isEqualTo(503)
         }
     }
+
+    private fun to3dRange(s: String) = toBrick(s, -1).locations
 
     @Nested
     inner class Part2 {
 
         @Test
-        internal fun `fixme`() {
+        internal fun `example 1`() {
             expectThat(1).isEqualTo(1)
         }
 
@@ -122,4 +130,8 @@ class Day22Test {
 
     private val input by lazy { readFile("/input-day22.txt")}
 
+}
+
+private fun DroppedBrick.toSimpleBrick(): Brick {
+    return Brick(this.locations, this.id)
 }
