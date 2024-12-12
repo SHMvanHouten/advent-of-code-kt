98b2be15c7c70de722415dbd84413ddd739bb5a3
Sjoerd <svanhouten@gmail.com>
2024-12-12 15:22:37 +0100

    undo deleting day 7 tests

--- kt2024/src/test/kotlin/com/github/shmvanhouten/adventofcode2024/day07/Day07Test.kt
+++ kt2024/src/test/kotlin/com/github/shmvanhouten/adventofcode2024/day07/Day07Test.kt
@@ -3,22 +3,54 @@ package com.github.shmvanhouten.adventofcode2024.day07
 import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
 
 class Day07Test {
 
     @Nested
     inner class Part1 {
 
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "190: 10 19",
+                "29: 10 19",
+                "3267: 81 40 27",
+                "292: 11 6 16 20"
+            ]
+        )
+        fun `valid sums can be obtained by multiplying and adding`(line: String) {
+            expectThat(isAValidLine(line)).isTrue()
+        }
+
+        @ParameterizedTest
+        @ValueSource(
+            strings = [
+                "83: 17 5",
+                "156: 15 6",
+                "7290: 6 8 6 15",
+                "161011: 16 10 13",
+                "192: 17 8 14",
+                "21037: 9 7 18 13",
+            ]
+        )
+        fun `invalid sums`(line: String) {
+            expectThat(isAValidLine(line)).isFalse()
+        }
+
         @Test
-        internal fun `fixme`() {
-            expectThat(1).isEqualTo(1)
+        internal fun `example 1`() {
+            expectThat(sumValidEquations(example)).isEqualTo(3749)
         }
 
         @Test
         internal fun `part 1`() {
-            expectThat(1).isEqualTo(1)
+            expectThat(sumValidEquations(input)).isEqualTo(4122618559853)
         }
     }
 
@@ -26,16 +58,27 @@ class Day07Test {
     inner class Part2 {
 
         @Test
-        internal fun `fixme`() {
-            expectThat(1).isEqualTo(1)
+        internal fun `example p 2`() {
+            expectThat(sumValidEquationsIncludingConcat(example)).isEqualTo(11387)
         }
 
         @Test
         internal fun `part 2`() {
-            expectThat(1).isEqualTo(1)
+            expectThat(sumValidEquationsIncludingConcat(input)).isEqualTo(227615740238334)
         }
     }
 
     private val input by lazy { readFile("/input-day07.txt")}
+    private val example = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+    """.trimIndent()
 
 }
