03a5933d0de7d83bc5e01ca0c106c26cf32e7ac2
Sjoerd <svanhouten@gmail.com>
2024-12-14 11:00:58 +0100

    day 14 pt 2

--- kt2024/src/main/kotlin/com/github/shmvanhouten/adventofcode2024/day14/Placeholder14.kt
+++ kt2024/src/main/kotlin/com/github/shmvanhouten/adventofcode2024/day14/Placeholder14.kt
@@ -4,6 +4,8 @@ import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
 import com.github.shmvanhouten.adventofcode.utility.collectors.product
 import com.github.shmvanhouten.adventofcode.utility.coordinate.Coordinate
 import com.github.shmvanhouten.adventofcode.utility.coordinate.toCoordinate
+import com.github.shmvanhouten.adventofcode.utility.grid.Grid
+import com.github.shmvanhouten.adventofcode.utility.grid.boolGridFromCoordinates
 import com.github.shmvanhouten.adventofcode.utility.strings.words
 
 fun main() {
@@ -11,7 +13,7 @@ fun main() {
         .lines()
         .map { toRobot(it) }
 //    robots.onEach(::println)
-    val moved = moveRobots(100, 101, 103, robots)
+    val moved = moveRobots(1000000, 101, 103, robots)
     println(quadrants(101, 103).map { it.countRobots(moved) }.onEach { println(it) }.product())
 }
 
@@ -40,8 +42,34 @@ fun quadrants(width: Int, height: Int): List<Quadrant> {
 fun moveRobots(times: Int, width: Int, height: Int, robots: List<Robot>): List<Robot> {
     return generateSequence(robots) { bots ->
         bots.map { it.move(width, height) }
-    }.drop(times).first()
+    }
+        .onEachIndexed {index, bots ->
+            if(index % 10000 == 0) {
+                print(index)
+            }
+            printBotsIfInFormation(bots, index)
+        }
+        .drop(times).first()
 }
+
+private fun printBotsIfInFormation(
+    bots: List<Robot>,
+    index: Int
+) {
+    val grid = boolGridFromCoordinates(bots.map(Robot::position))
+    val drawn = grid.draw()
+    if (drawn.contains("██████████")) {
+        println(drawn)
+        println("at attempt $index")
+    }
+}
+
+//private fun bigClusterIsFormed(
+//    bots: List<Robot>,
+//    grid: Grid<Boolean>
+//) = bots.asSequence()
+//    .filter { (pos, _) -> pos.getSurrounding().any { grid.getOrNull(it) == true } }
+//    .takeIf { it }
 
 fun toRobot(input: String): Robot {
     val (position, velocity) = input.words().map { it.substringAfter("=") }
@@ -74,3 +102,8 @@ data class Quadrant(
     }
 }
 
+fun Grid<Boolean>.draw(): String {
+    return this.rows().map { row ->
+        row.map { if(it) '█' else ' ' }.joinToString("")
+    }.joinToString("\n")
+}
