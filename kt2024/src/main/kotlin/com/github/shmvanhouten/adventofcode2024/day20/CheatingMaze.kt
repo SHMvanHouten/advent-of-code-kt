9e143082f72d1cbc4f5736e856247e02b363632e
Sjoerd <svanhouten@gmail.com>
2024-12-22 21:18:07 +0100

    day 20 works better for part 1 but still not good enough for part 2

--- kt2024/src/main/kotlin/com/github/shmvanhouten/adventofcode2024/day20/CheatingMaze.kt
+++ kt2024/src/main/kotlin/com/github/shmvanhouten/adventofcode2024/day20/CheatingMaze.kt
@@ -4,20 +4,20 @@ import com.github.shmvanhouten.adventofcode.utility.coordinate.Coordinate
 import com.github.shmvanhouten.adventofcode.utility.grid.Grid
 import com.github.shmvanhouten.adventofcode.utility.grid.charGrid
 import java.util.*
+import kotlin.math.abs
 
-fun findCheats(input: String, cheatLength: Int = 2): List<CheatingResult> {
+fun findCheats(input: String, cheatLength: Int = 2): Sequence<CheatingResult> {
     val grid = charGrid(input)
     val start = grid.firstLocationOf { it == 'S' }!!
     val goal = grid.firstLocationOf { it == 'E' }!!
 
     val quickestPath = quickestPath(grid, Path(listOf(start)), goal)!!
     return possibleCheatsAlongPath(grid, quickestPath, cheatLength)
-        .map { it to grid.applyCheat(it) }
-        .mapNotNull { (cheat, grid) ->
-            val pathWithRemovedWall = Path(quickestPath.steps.sublistUntil(cheat.stepToBlock) + cheat.removedWalls.first())
-            val pathFromCheat = quickestPath(grid, pathWithRemovedWall, goal, quickestPath)
-            if(pathFromCheat  != null){
-                toCheatingResult(cheat, grid, pathFromCheat, quickestPath.length)
+        .asSequence()
+        .mapNotNull { cheat ->
+            val pathFromCheat = quickestPath(grid, Path(cheat.pathWithCheat), goal, quickestPath)
+            if (pathFromCheat != null) {
+                CheatingResult(cheat, pathFromCheat, quickestPath.length)
             } else null
         }.filter { it.gain > 0 }
 }
@@ -25,16 +25,21 @@ fun findCheats(input: String, cheatLength: Int = 2): List<CheatingResult> {
 // starting position of the cheat is the point on the path we start from
 fun possibleCheatsAlongPath(grid: Grid<Char>, quickestPath: Path, cheatLength: Int): List<Cheat> {
     val steps = quickestPath.steps
-    return steps.subList(0, steps.lastIndex).asSequence()
-        .flatMap { step -> adjacentInnerWalls(step, grid).map { step to it } }
-        .map { (step, wall) -> Cheat(listOf(wall), steps[steps.indexOf(step) + 1]) }
+    return steps.subList(0, steps.lastIndex).withIndex().asSequence()
+        .map { (index, step) -> step to quickestPath.steps.subList(0, index + 1) }
+        .flatMap { (step, pathSoFar) -> grid.listAllOpenSpotsWithIn(step, cheatLength, pathSoFar.toSet())
+            .map { pathSoFar + step to it }}
+        .map { (pathSoFar, warp) -> Cheat(pathSoFar.dropLast(2) + directPath(pathSoFar.last(), warp), steps[pathSoFar.lastIndex]) }
         .toList()
 }
 
-fun adjacentInnerWalls(loc: Coordinate, grid: Grid<Char>): List<Coordinate> {
-    return loc.getSurroundingManhattan()
-        .filter { grid[it] == '#' }
-        .filter { !grid.isOnPerimiter(it) } // todo: can be more efficient
+fun directPath(starting: Coordinate, warp: Coordinate): List<Coordinate> {
+    val (startX, endX) = listOf(starting.x, warp.x).sorted()
+    val (startY, endY) = listOf(starting.y, warp.y).sorted()
+    val walkDown = (startY..endY).map { y -> Coordinate(startX, y) }
+    val cheatWalk = walkDown + (startX..endX).drop(1).map { x -> Coordinate(x, endY) }
+    return if(cheatWalk.first() == starting) cheatWalk
+    else cheatWalk.reversed()
 }
 
 fun quickestPath(input: String): Path {
@@ -55,7 +60,7 @@ private fun quickestPath(
     while (unfinishedPaths.isNotEmpty()) {
         val path = unfinishedPaths.poll()
         val lastLoc = path.lastLoc
-        if(lastLoc in existingPath.steps) return path.dropLast() + existingPath.pathFrom(lastLoc)
+        if(lastLoc in existingPath) return path.dropLast() + existingPath.pathFrom(lastLoc)
         if (lastLoc == goal) return path
         unfinishedPaths.addAll(path.nextSteps(grid)
             .map { path.stepTo(it) })
@@ -73,6 +78,7 @@ data class Path(
     val length = steps.size - 1
     val lastLoc = steps.lastOrNull()?:Coordinate(-1,-1)
     private val beforeLastLoc = steps.getOrNull(steps.lastIndex - 1)
+    private val stepsAsSet: Set<Coordinate> by lazy { steps.toSet() }
     override fun compareTo(other: Path): Int = other.length.compareTo(this.length)
 
     fun nextSteps(grid: Grid<Char>): List<Coordinate> {
@@ -97,10 +103,12 @@ data class Path(
         return Path(steps + other.steps)
     }
 
+    operator fun contains(lastLoc: Coordinate): Boolean = lastLoc in stepsAsSet
+
 }
 
 data class Cheat(
-    val removedWalls: List<Coordinate>,
+    val pathWithCheat: List<Coordinate>,
     val stepToBlock: Coordinate
 ) {
     override fun equals(other: Any?): Boolean {
@@ -109,51 +117,55 @@ data class Cheat(
 
         other as Cheat
 
-        return removedWalls == other.removedWalls
+        return pathWithCheat == other.pathWithCheat
     }
 
     override fun hashCode(): Int {
-        return removedWalls.hashCode()
-    }
-
-    fun getStep(coordinate: Coordinate): Int? {
-        return removedWalls.withIndex().find { it.value == coordinate }?.index
+        return pathWithCheat.hashCode()
     }
 
 }
 
 fun priorityQueueOf(path: Path): PriorityQueue<Path> = PriorityQueue<Path>(listOf(path))
 
-fun Grid<Char>.applyCheat(cheat: Cheat): Grid<Char> {
-    return Grid(grid.mapIndexed { y, row ->
-        row.mapIndexed { x, c ->
-            val removedWall = cheat.getStep(Coordinate(x, y))
-            if(removedWall != null) removedWall.toChar()
-            else if(cheat.stepToBlock == Coordinate(x, y)) '#'
-            else c
-        }
-    })
+//fun Grid<Char>.applyCheat(cheat: Cheat): Grid<Char> {
+//    return Grid(grid.mapIndexed { y, row ->
+//        row.mapIndexed { x, c ->
+//            val removedWall = cheat.getStep(Coordinate(x, y))
+//            if(removedWall != null) removedWall.toChar()
+//            else if(cheat.stepToBlock == Coordinate(x, y)) '#'
+//            else c
+//        }
+//    })
+//
+//}
 
-}
-
-data class CheatingResult(val cheat: Cheat, val grid: Grid<Char>, val quickestPath: Path, private val lengthToBeat: Int) {
+data class CheatingResult(val cheat: Cheat, val quickestPath: Path, private val lengthToBeat: Int) {
     val gain = lengthToBeat - quickestPath.length
     override fun toString(): String {
         return cheat.toString() + quickestPath.length
     }
-    fun print(): String {
+    fun print(grid: Grid<Char>): String {
         val mutableGrid = grid.toMutableGrid()
         quickestPath.steps.forEach { mutableGrid[it] = '0' }
-        cheat.removedWalls.forEachIndexed { i, c -> mutableGrid[c] = i.toChar() }
+        cheat.pathWithCheat.forEachIndexed { i, c -> mutableGrid[c] = i.toChar() }
         return mutableGrid.toString()
     }
 }
 
-fun toCheatingResult(cheat: Cheat, grid: Grid<Char>, quickestPath: Path, lengthToBeat: Int): CheatingResult? {
-    return if (quickestPath != null) CheatingResult(cheat, grid, quickestPath, lengthToBeat)
-    else null
-}
-
 fun <T> List<T>.sublistUntil(element: T): List<T> {
     return subList(0, indexOf(element))
+}
+
+fun Grid<Char>.listAllOpenSpotsWithIn(start: Coordinate, length: Int, mustNotEndIn: Set<Coordinate>): Set<Coordinate> {
+    val startY = maxOf(start.y - length, 0)
+    val endY = minOf(start.y + length, this.height - 1)
+    return (startY..endY).flatMap { y ->
+        val availableSteps = length - abs(start.y - y)
+        val startX = maxOf(start.x - availableSteps, 0)
+        val endX = minOf(start.x + availableSteps, this.width - 1)
+        (startX..endX).map{x -> Coordinate(x, y)}
+            .filter { this[it] != '#' }
+            .filter { it !in mustNotEndIn }
+    }.toSet()
 }
--- kt2024/src/test/kotlin/com/github/shmvanhouten/adventofcode2024/day20/Day20Test.kt
+++ kt2024/src/test/kotlin/com/github/shmvanhouten/adventofcode2024/day20/Day20Test.kt
@@ -1,6 +1,9 @@
 package com.github.shmvanhouten.adventofcode2024.day20
 
 import com.github.shmvanhouten.adventofcode.utility.FileReader.readFile
+import com.github.shmvanhouten.adventofcode.utility.coordinate.Coordinate
+import com.github.shmvanhouten.adventofcode.utility.grid.Grid
+import com.github.shmvanhouten.adventofcode.utility.grid.charGrid
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import strikt.api.expect
@@ -21,14 +24,14 @@ class Day20Test {
         @Test
         fun `there is a cheat for the example that saves 64 ps`() {
             val cheats = findCheats(example)
-            expectThat(cheats.filter{it.gain == 64}).hasSize(1)
+            expectThat(cheats.filter{it.gain == 64}.toList()).hasSize(1)
         }
 
         @Test
         fun `there are 44 cheats that save time for the example`() {
-            val findCheats = findCheats(example)
+            val findCheats = findCheats(example).toList()
             expect {
-//                that(findCheats.filter { it.gain == 2 }).hasSize(14)
+                that(findCheats.filter { it.gain == 2 }).hasSize(14)
                 that(findCheats.filter { it.gain == 4 }).hasSize(14)
                 that(findCheats.filter{ it.gain == 6 }).hasSize(2)
                 that(findCheats.filter{ it.gain == 8 }).hasSize(4)
@@ -45,7 +48,7 @@ class Day20Test {
         @Test
         internal fun `part 1`() {
             val cheats = findCheats(input)
-            expectThat(cheats.filter { it.gain >= 100 }).hasSize(1445)
+            expectThat(cheats.filter { it.gain >= 100 }.toList()).hasSize(1445)
         }
     }
 
@@ -53,16 +56,64 @@ class Day20Test {
     inner class Part2 {
 
         @Test
-        internal fun `fixme`() {
-            val cheats = findCheats(example, cheatLength = 20)
-            expectThat(cheats.filter{it.gain == 76}).hasSize(1)
+        fun `list all open spots within cheat length`() {
+            val grid = charGrid(example)
+            val openSpots = grid.listAllOpenSpotsWithIn(Coordinate(1, 3), 6, setOf(Coordinate(1,3)))
+            expectThat(print(grid, openSpots)).isEqualTo(
+                """
+                    ###############
+                    #uuu#u..#.....#
+                    #u#u#u#.#.###.#
+                    #S#uuu#u#.#...#
+                    #######.#.#.###
+                    #######.#.#...#
+                    #######.#.###.#
+                    ###u.E#...#...#
+                    ###.#######.###
+                    #u..###...#...#
+                    #.#####.#.###.#
+                    #.#...#.#.#...#
+                    #.#.#.#.#.#.###
+                    #...#...#...###
+                    ###############
+                """.trimIndent()
+            )
+
+        }
+
+        @Test
+        internal fun `example 1 cheats`() {
+            val cheats = findCheats(example, cheatLength = 20).toList()
+            println(cheats.filter { it.gain == 76 }.map { it.print(charGrid(example)) }.joinToString("\n"))
+            expect {
+                that(cheats.filter{it.gain == 50 }).hasSize(32)
+                that(cheats.filter{it.gain == 52 }).hasSize(31)
+                that(cheats.filter{it.gain == 54 }).hasSize(29)
+                that(cheats.filter{it.gain == 56 }).hasSize(39)
+                that(cheats.filter{it.gain == 58 }).hasSize(25)
+                that(cheats.filter{it.gain == 60 }).hasSize(23)
+                that(cheats.filter{it.gain == 62 }).hasSize(20)
+                that(cheats.filter{it.gain == 64 }).hasSize(19)
+                that(cheats.filter{it.gain == 66 }).hasSize(12)
+                that(cheats.filter{it.gain == 68 }).hasSize(14)
+                that(cheats.filter{it.gain == 70 }).hasSize(12)
+                that(cheats.filter{it.gain == 72 }).hasSize(22)
+                that(cheats.filter{it.gain == 74 }).hasSize(4)
+                that(cheats.filter{it.gain == 76}).hasSize(3)
+            }
         }
 
         @Test
         internal fun `part 2`() {
             val cheats = findCheats(input, 20)
-            expectThat(cheats.filter { it.gain >= 100 }).hasSize(1445)
+            expectThat(cheats.map { it.gain }.filter { it >= 100 }.toList()).hasSize(1445)
         }
+    }
+
+    fun print(grid: Grid<Char>, specialCoordinates: Set<Coordinate>): String {
+        val mutableGrid = grid.toMutableGrid()
+        specialCoordinates.forEach { mutableGrid[it] = 'u' }
+        return mutableGrid.toString()
     }
 
     private val input by lazy { readFile("/input-day20.txt")}
